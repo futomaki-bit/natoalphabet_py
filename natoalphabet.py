acfd1792fa90e417a1c8679b98fc74107c10ac57
futomakiyoin <futomakiyoin@gmail.com>
2021-06-12 21:21:55 -0400

    Small description of code

--- natoalphabet.py
+++ natoalphabet.py
@@ -1,3 +1,4 @@
+# Program to convert word into nato alphabet
 from string import *
 
 print("Convert your word into nato alphabet")
@@ -6,37 +7,39 @@ print()
 
 def nato(letter):
     return{
-        "a" : "Alfa",
-        "b" : "Bravo",
-        "c" : "Charlie",
-        "d" : "Delta",
-        "e" : "Echo",
-        "f" : "Foxtrot",
-        "g" : "Golf",
-        "h" : "Hotel",
-        "i" : "India",
-        "j" : "Juliett",
-        "k" : "Kilo",
-        "l" : "Lima",
-        "m" : "Mike",
-        "n" : "November",
-        "o" : "Oscar",
-        "p" : "Papa",
-        "q" : "Quebec",
-        "r" : "Romeo",
-        "s" : "Sierra",
-        "t" : "Tango",
-        "u" : "Uniform",
-        "v" : "Victor",
-        "w" : "Whiskey",
-        "x" : "Xray",
-        "y" : "Yankee",
-        "z" : "Zulu",
-        " " : ""
+        "a": "Alfa",
+        "b": "Bravo",
+        "c": "Charlie",
+        "d": "Delta",
+        "e": "Echo",
+        "f": "Foxtrot",
+        "g": "Golf",
+        "h": "Hotel",
+        "i": "India",
+        "j": "Juliett",
+        "k": "Kilo",
+        "l": "Lima",
+        "m": "Mike",
+        "n": "November",
+        "o": "Oscar",
+        "p": "Papa",
+        "q": "Quebec",
+        "r": "Romeo",
+        "s": "Sierra",
+        "t": "Tango",
+        "u": "Uniform",
+        "v": "Victor",
+        "w": "Whiskey",
+        "x": "Xray",
+        "y": "Yankee",
+        "z": "Zulu",
+        " ": ""
     }[letter]
+
 
 def printnato(letter):
     print(nato(letter.lower()))
+
 
 print(word + " in nato alphabet:")
 for letter in word:
